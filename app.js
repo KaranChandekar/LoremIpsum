b5c1c07899fd6712768191c3049b0178230170e1
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-18 19:57:56 +0530

    add event listener

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
 const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const value = parseInt(amount.value);
+  const random = Math.floor(Math.random() * text.length);
+
+  // empty
+  // -1
+  // > 9
+  if (isNaN(value) || value < 0 || value > 9) {
+    result.innerHTML = `<p class='result'>${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText
+      .map(function (item) {
+        return `<p class="result">${item}</p>`;
+      })
+      .join("");
+    result.innerHTML = tempText;
+  }
+});
